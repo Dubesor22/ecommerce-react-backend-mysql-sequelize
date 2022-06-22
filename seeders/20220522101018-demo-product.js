0cb80922c5c643cb771fc23c60cf77b8ad064de2
Dubesor22 <dubesor22@gmail.com>
2022-06-23 00:21:35 +0200

    merge

--- seeders/20220522101018-demo-product.js
+++ seeders/20220522101018-demo-product.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -10,28 +10,156 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-    return queryInterface.bulkInsert('products', [
-      { name: 'Product 1', price: 11, description: 'Description of product 1', image: 'f3647c7ef40feb54dd85f30ceef2546c', createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 2', price: 22, description: 'Description of product 2', image: 'd091e08ed268104295734bfa7cb2c2d1', createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 3', price: 33, description: 'Description of product 3', image: 'a83c4180a0f1030e7844ab4109607e1c', createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 4', price: 44, description: 'Description of product 4',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 5', price: 55, description: 'Description of product 5',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 6', price: 66, description: 'Description of product 6',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 7', price: 77, description: 'Description of product 7',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 8', price: 88, description: 'Description of product 8',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 9', price: 99, description: 'Description of product 9',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 10', price: 10, description: 'Description of product 10',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 11', price: 21, description: 'Description of product 11',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 12', price: 32, description: 'Description of product 12',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 13', price: 43, description: 'Description of product 13',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 14', price: 54, description: 'Description of product 14',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 15', price: 65, description: 'Description of product 15',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 16', price: 76, description: 'Description of product 16',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 17', price: 87, description: 'Description of product 17',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 18', price: 98, description: 'Description of product 18',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 19', price: 19, description: 'Description of product 19',createdAt: new Date(), updatedAt: new Date() },
-      { name: 'Product 20', price: 20, description: 'Description of product 20',createdAt: new Date(), updatedAt: new Date() }
+     */
+    return queryInterface.bulkInsert("products", [
+      {
+        name: "Product 1",
+        price: 11,
+        description: "Description of product 1",
+        image:
+          "https://cdn1.dotesports.com/wp-content/uploads/sites/4/2021/11/15155110/ezgif-2-b0fc567224a3-1.jpg",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 2",
+        price: 22,
+        description: "Description of product 2",
+        image:
+          "https://cdn1.dotesports.com/wp-content/uploads/sites/4/2021/11/15155110/ezgif-2-b0fc567224a3-1.jpg",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 3",
+        price: 33,
+        description: "Description of product 3",
+        image:
+          "https://cdn1.dotesports.com/wp-content/uploads/sites/4/2021/11/15155110/ezgif-2-b0fc567224a3-1.jpg",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 4",
+        price: 44,
+        image:
+          "https://cdn1.dotesports.com/wp-content/uploads/sites/4/2021/11/15155110/ezgif-2-b0fc567224a3-1.jpg",
+        description: "Description of product 4",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 5",
+        price: 55,
+        description: "Description of product 5",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 6",
+        price: 66,
+        description: "Description of product 6",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 7",
+        price: 77,
+        description: "Description of product 7",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 8",
+        price: 88,
+        description: "Description of product 8",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 9",
+        price: 99,
+        description: "Description of product 9",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 10",
+        price: 10,
+        description: "Description of product 10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 11",
+        price: 21,
+        description: "Description of product 11",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 12",
+        price: 32,
+        description: "Description of product 12",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 13",
+        price: 43,
+        description: "Description of product 13",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 14",
+        price: 54,
+        description: "Description of product 14",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 15",
+        price: 65,
+        description: "Description of product 15",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 16",
+        price: 76,
+        description: "Description of product 16",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 17",
+        price: 87,
+        description: "Description of product 17",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 18",
+        price: 98,
+        description: "Description of product 18",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 19",
+        price: 19,
+        description: "Description of product 19",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Product 20",
+        price: 20,
+        description: "Description of product 20",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ]);
   },
 
@@ -42,5 +170,5 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
